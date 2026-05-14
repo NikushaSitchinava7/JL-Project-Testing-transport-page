a9b6a6d95eb92c8676d1bd5adbf66acb2f891069
nikushasitchinava <nikushasitchinava777@gmail.com>
2026-05-14 15:29:45 +0400

    Javascripti Update header+cards interactive
    ho magram ro davarefreshe is gverdi tkt ge ze ragac isev Shecvales mokled isev axlidan fotoebi 😁

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const categorybar = document.querySelector(".category");
+const cards = document.querySelectorAll(".card");
+let scrollposition = 0;
 
+
+
+// vistvis vwer am komentarebs revici revici
+// hederis scrolvis kodi
+window.addEventListener("scroll", () => {
+    const currentscroll = window.scrollY;
+    // chemi amjamindeli lokacia
+
+    // kodi rodis chaomvides/avides anu hederi
+    if(currentscroll > scrollposition && currentscroll > 140){
+        categorybar.classList.add("scrolldown")
+    }else{
+        categorybar.classList.remove("scrolldown")
+    }
+
+    // da douyenebs mere amasaaa xd
+    scrollposition = currentscroll;
+});
+
+
+// yvela cards gadaxedavs
+cards.forEach(card => {
+    // moausi miusvhira
+    card.addEventListener("mouseenter", () => {
+        card.classList.add("hovered")
+    })
+    // mausi gadavida am cardidan
+    card.addEventListener("mouseleave", () => {
+        card.classList.remove("hovered")
+    })
+});
